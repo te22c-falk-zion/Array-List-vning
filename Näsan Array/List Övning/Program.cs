e81a635806fab746ed58b74f2686c81f4b61d525
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-11-07 10:52:31 +0100

    first commit

---
+++ Näsan Array/List Övning/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string[] toyNames = {"Beyblade", "Bakugan", "Lego", "Digimon", "Pokemon TCG"};
+string[] names = {"Don Quixote","Ishmael", "Yi sang", "Charon", "Faust"};
+int[] ints = {10, 4, 8, 6, 4};
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"{names[i]} picked up a {toyNames[i]} and gave it a {ints[i]}");
+    Console.ReadLine();
+}
